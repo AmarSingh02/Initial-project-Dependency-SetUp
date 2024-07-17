ca3134a5c0a329db11c5d70a4617996f565d48c0
Amar singh <asingh58885@gmail.com>
2024-07-17 22:35:24 +0530

    docs added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 
 const App = () => {
   return <div>
-<h1 className="text-center">
+<h1 className="text-center mt-5" style={{color:"blue", fontWeight:'bold'}}>
 Hey Dev's
 </h1>
-  
+<h4 className="text-center mt-4 mb-4"> React Dependency Initial Setup</h4>
+<div className="text-center">
+
+
+<p>To run npm  <strong>npm run start:preprod</strong> </p>
+<p>To run npm  <strong>npm run start:production</strong> </p>
+<p>To run build  <strong>npm run build:preprod</strong> </p>
+<p>To run build  <strong>npm run build:production</strong> </p>
+<h4>More Info Visit to package.json file and Environment.js </h4>
+</div>
 
   </div>;
 };
